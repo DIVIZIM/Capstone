ea99c91ee7cba40914246e92473979d114eafa76
DIVIZIM <rejectedcritisizm@gmail.com>
2020-10-17 11:56:11 -0500

    Updated the array section with variable names that match the data sheet

--- array.py
+++ array.py
@@ -3,75 +3,77 @@ import time
 from enum import Enum
 
 class OutputY(Enum):
-    O1 = 5
-    O2 = 6
-    O3 = 13
+    A = 5
+    B = 6
+    C = 13
     
 class OutputX(Enum):
-    O1 = 17
-    O2 = 27
-    O3 = 22
+    A = 17
+    B = 27
+    C = 22
     
-SET = 12
-OUTPUT = 16
+G = 12
+D = 16
 
 HIGH = 1
 LOW = 0
 
 def decimalToBinary(n):
-    return int(bin(n).replace("0b", ""))
+    return bin(n).replace("0b", "").zfill(3)
 
 def initilize():
     wiringpi.wiringPiSetup()
-    wiringpi.pinMode(OutputY.O1.value, HIGH)
-    wiringpi.pinMode(OutputY.O2.value, HIGH)
-    wiringpi.pinMode(OutputY.O3.value, HIGH)
-    wiringpi.pinMode(OutputX.O1.value, HIGH)
-    wiringpi.pinMode(OutputX.O2.value, HIGH)
-    wiringpi.pinMode(OutputX.O3.value, HIGH)
-    wiringpi.pinMode(SET, HIGH)
-    wiringpi.pinMode(OUTPUT, HIGH)
+    wiringpi.pinMode(OutputY.A.value, HIGH)
+    wiringpi.pinMode(OutputY.B.value, HIGH)
+    wiringpi.pinMode(OutputY.C.value, HIGH)
+    wiringpi.pinMode(OutputX.A.value, HIGH)
+    wiringpi.pinMode(OutputX.B.value, HIGH)
+    wiringpi.pinMode(OutputX.C.value, HIGH)
+    wiringpi.pinMode(G, HIGH)
+    wiringpi.pinMode(D, HIGH)
     
-    wiringpi.digitalWrite(OutputY.O1.value, LOW)
-    wiringpi.digitalWrite(OutputY.O2.value, LOW)
-    wiringpi.digitalWrite(OutputY.O3.value, LOW)
-    wiringpi.digitalWrite(OutputX.O1.value, LOW)
-    wiringpi.digitalWrite(OutputX.O2.value, LOW)
-    wiringpi.digitalWrite(OutputX.O3.value, LOW)
-    wiringpi.digitalWrite(SET, LOW)
-    wiringpi.digitalWrite(OUTPUT, LOW)
+    wiringpi.digitalWrite(OutputY.A.value, LOW)
+    wiringpi.digitalWrite(OutputY.B.value, LOW)
+    wiringpi.digitalWrite(OutputY.C.value, LOW)
+    wiringpi.digitalWrite(OutputX.A.value, LOW)
+    wiringpi.digitalWrite(OutputX.B.value, LOW)
+    wiringpi.digitalWrite(OutputX.C.value, LOW)
+    wiringpi.digitalWrite(G, HIGH)
+    wiringpi.digitalWrite(D, LOW)
 
 #2D vector for location of array
-# 012
+# 01
 #0
 #1
-#2
+#
+#(ABC,ABC)
+#(000,000)
 def setElement(element,value):
     (x,y) = element
     x = decimalToBinary(x)
     y = decimalToBinary(y)
     
+    i = 0
     for z in OutputX:
-        if(x%10 == 0):
+        if(x[i] == 0):
             wiringpi.digitalWrite(z.value,0)
-            print(x)
         else:
             wiringpi.digitalWrite(z.value,1)
-            print(x)
-        x = int(x/10)
+        i += 1
         
+    i = 0
     for z in OutputY:
-        if(y%10 == 0):
+        if(y[i] == 0):
             wiringpi.digitalWrite(z.value,0)
         else:
             wiringpi.digitalWrite(z.value,1)
-        y = int(y/10)
+        i += 1
         
     #Pulse the output to turn on/off a selected aray element
-    wiringpi.digitalWrite(OUTPUT,value)
-    wiringpi.digitalWrite(SET, 1)
+    wiringpi.digitalWrite(D,value)
+    wiringpi.digitalWrite(G, LOW)
     time.sleep(.01) #wait 10 ms for signal to propogate
-    wiringpi.digitalWrite(SET, 0)
+    wiringpi.digitalWrite(G, HIGH)
     
 initilize()
 setElement((3,2),1)
